fdc825a1edaecd0e91d69711918a5df731426c32
youssefkhalil10 <youssefkhalil510@gmail.com>
2025-10-25 13:23:33 +0300

    before deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Gaming-City/",
+  base: import.meta.env.MODE === "production" ? "/Gaming-City/" : "/",
   server: {
     proxy: {
       "/api": {
