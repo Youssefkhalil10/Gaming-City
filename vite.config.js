39e8323e840ea85ceeac2be34b5ff4a012eaf8e7
youssefkhalil10 <youssefkhalil510@gmail.com>
2025-10-25 12:39:31 +0300

    Update before deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,
-    tailwindcss()
-  ],
-   server: {
+  plugins: [react(), tailwindcss()],
+  base: "/Gaming-City/",
+  server: {
     proxy: {
       "/api": {
         target: "https://gamingcity-production-48e1.up.railway.app",
@@ -16,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
