0662afb4cb5ea7588214e5a2bae4523bd48e2c74
youssefkhalil10 <youssefkhalil510@gmail.com>
2025-11-04 14:41:21 +0200

    fix vercel routing

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  base: mode === "production" ? "/Gaming-City/" : "/",
+  base: "/",
 
   build: {
     outDir: "dist",
@@ -20,4 +20,4 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-}));
+});
